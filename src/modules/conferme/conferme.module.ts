4a15c3c95b4da374ce6e41409ddde3538b4d16cc
Davide <rondey90@gmail.com>
2025-09-04 21:30:56 +0200

    Before creation of conferme, added check for esame exists in a particular ambulatorio

--- src/modules/conferme/conferme.module.ts
+++ src/modules/conferme/conferme.module.ts
@@ -3,9 +3,10 @@ import { ConfermeService } from './conferme.service';
 import { ConfermeController } from './conferme.controller';
 import { Conferma } from './entities/conferma.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EsameAmbulatorio } from '../esami/entities/esame-ambulatorio.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Conferma])],
+  imports: [TypeOrmModule.forFeature([Conferma, EsameAmbulatorio])],
   controllers: [ConfermeController],
   providers: [ConfermeService],
 })
--- src/modules/conferme/conferme.service.ts
+++ src/modules/conferme/conferme.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Conferma } from './entities/conferma.entity';
 import { Repository } from 'typeorm';
 import { SortParams } from 'src/common/pipe/sort.pipe';
+import { EsameAmbulatorio } from '../esami/entities/esame-ambulatorio.entity';
 // import { UpdateConfermeDto } from './dto/update-conferme.dto';
 
 @Injectable()
@@ -11,14 +12,27 @@ export class ConfermeService {
   constructor(
     @InjectRepository(Conferma)
     private confermeRepository: Repository<Conferma>,
+    @InjectRepository(EsameAmbulatorio)
+    private esameAmbulatorioRepository: Repository<EsameAmbulatorio>,
   ) {}
 
   async create(createConfermeDto: CreateConfermeDto) {
+    // First of all, check if EsameAmbulatorio exists
+    const esameAmbulatorio =
+      await this.esameAmbulatorioRepository.existsBy(createConfermeDto);
+    if (!esameAmbulatorio)
+      throw new HttpException(
+        'Questo esame non esiste in questo ambulatorio',
+        HttpStatus.BAD_REQUEST,
+      );
+
+    // Then, check if Conferma already exists
     const conferma = await this.confermeRepository.findOneBy({
       esameAmbulatorio: createConfermeDto,
     });
     if (conferma)
       throw new HttpException('Conferma già esistente', HttpStatus.BAD_REQUEST);
+
     return this.confermeRepository.save(
       this.confermeRepository.create({
         esameAmbulatorio: createConfermeDto,
